3c15b8555f21ee3ecb3a3ea34fb1ce5b181301a8
Paul Evans <paul.d.evans+paulecoyote@gmail.com>
2012-03-03 23:44:14 -0600

    xunit 1.9 release

---
+++ Main/xunit.buildtasks/MakeTemplate.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+public class MakeTemplate : Task
+{
+    [Required]
+    public ITaskItem[] InputFiles { get; set; }
+
+    [Required]
+    public ITaskItem ZipFile { get; set; }
+
+    public override bool Execute()
+    {
+        string zipFilename = ZipFile.GetMetadata("FullPath");
+        string zipDateString = ZipFile.GetMetadata("ModifiedTime");
+        DateTime zipDate = zipDateString.Length == 0 ? DateTime.MinValue : DateTime.Parse(zipDateString);
+        bool build = false;
+
+        foreach (var item in InputFiles)
+            if (DateTime.Parse(item.GetMetadata("ModifiedTime")) > zipDate)
+            {
+                build = true;
+                break;
+            }
+
+        if (!build)
+            return true;
+
+        File.Delete(zipFilename);
+        Log.LogMessage("Creating template {0}", zipFilename);
+
+        using (var zipFile = new CodePlex.ZipLibrary.ZipFile(zipFilename))
+        {
+            foreach (var item in InputFiles)
+            {
+                string itemFilename = item.GetMetadata("FullPath");
+                string directoryPathInArchive = item.GetMetadata("OutputLocation") + item.GetMetadata("RecursiveDir");
+                zipFile.AddFile(itemFilename, directoryPathInArchive);
+            }
+
+            zipFile.Save();
+        }
+
+        return true;
+    }
+}
